d4fc3ce20734273ca4ae48a9cb6cc0c4ad100869
Jean-Dv <mrjunior127@gmail.com>
2023-08-18 12:18:08 -0500

    style: rewrite some files with eslint and prettier

--- __test__/home.spec.ts
+++ __test__/home.spec.ts
@@ -4,8 +4,7 @@ import { appServer, routePrefix } from '@root/bin/www'
 
 describe(`GET ${routePrefix}/ping`, () => {
   test('should return 200 and text pong', async () => {
-    const response = await request(appServer)
-      .get(`${routePrefix}/ping`)
+    const response = await request(appServer).get(`${routePrefix}/ping`)
     expect(response.statusCode).toBe(200)
     expect(response.body.message).toMatch('pong')
   })
--- apps/home/http.ts
+++ apps/home/http.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from 'express'
+import { type Request, type Response } from 'express'
 
 export class HomeHttpHandler {
   /**
-     * @param {Request|any} req -> Request of client
-     * @return {Response} res -> Response of server
-     */
-  getPing (_req: Request, res: Response): Response {
+   * @param {Request|any} req -> Request of client
+   * @return {Response} res -> Response of server
+   */
+  getPing(_req: Request, res: Response): Response {
     return res.status(200).json({
       ok: true,
       message: 'pong'
